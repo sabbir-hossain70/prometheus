5aea5e050e18b7e8393eea31df25e70cc65118e1
Sabbir <sabbir@appscode.com>
2024-05-29 11:40:00 +0600

    First commit

---
+++ api/api.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+)
+
+var pingCounter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "api_ping_count",
+		Help: "Number of ping requests",
+	})
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	pingCounter.Inc()
+	fmt.Fprintf(w, "pong")
+}
+
+func RunServer(Port string) {
+	fmt.Println("Inside RunServer")
+	prometheus.MustRegister(pingCounter)
+	http.HandleFunc("/ping", ping)
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":"+Port, nil)
+}
